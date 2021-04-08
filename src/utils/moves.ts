0bea5b5af3c955bf4d0487bd8ba0c2f1e68fb58a
Filipe Pfluck <filipepfluckdev@gmail.com>
2021-04-08 15:37:41 -0300

    finished moves list

---
+++ src/utils/moves.ts
@@ -0,0 +1,537 @@
+const moves = {
+    pawn: {
+        1: {
+            moves: {
+                up: 1,
+                type: 'continuous'
+            }
+        },
+        2: {
+            moves: {
+                up: 1,
+                upRight: 1,
+                upLeft: 1,
+                type: 'continuous'
+            }
+        },
+        3: {
+            moves: {
+                up: 1,
+                upRight: 1,
+                upLeft: 1,
+                downRight: 1,
+                downLeft: 1,
+                type: 'continuous'
+            }
+        }
+    },
+
+    majorGeneral: {
+        1: {
+            moves: {
+                up: 1,
+                upRight: 1,
+                upLeft: 1,
+                type: 'continuous'
+            }
+        },
+        2: {
+            moves: {
+                up: 1,
+                upRight: 1,
+                upLeft: 1,
+                downRight: 1,
+                downLeft: 1,
+                type: 'continuous'
+            }
+        },
+        3: {
+            moves: {
+                up: 1,
+                upRight: 1,
+                upLeft: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                type: 'continuous'
+            }
+        }
+    },
+
+    lieutenantGeneral: {
+        1: {
+            moves: {
+                up: 1,
+                upRight: 1,
+                upLeft: 1,
+                downRight: 1,
+                downLeft: 1,
+                type: 'continuous'
+            }
+        },
+        2: {
+            moves: {
+                up: 1,
+                upRight: 1,
+                upLeft: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                type: 'continuous'
+            }
+        },
+        3: {
+            moves: {
+                up: 2, 
+                upRight: 1,
+                upLeft: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                downRight: 1,
+                downLeft: 1,
+                type: 'continuous'
+            }
+        }
+    },
+
+    general: {
+        1: {
+            moves: {
+                up: 1,
+                upRight: 1,
+                upLeft: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                type: 'continuous'
+            }
+        },
+        2: {
+            moves: {
+                up: 2, 
+                upRight: 1,
+                upLeft: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                downRight: 1,
+                downLeft: 1,
+                type: 'continuous'
+            }
+        },
+        3: {
+            moves: {
+                up: 2, 
+                upRight: 2,
+                upLeft: 2,
+                left: 2,
+                right: 2,
+                down: 2,
+                downRight: 2,
+                downLeft: 2,
+                type: 'continuous'
+            }
+        }
+    },
+
+    archer: {
+        1: {
+            moves: {
+                up: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                type: 'continuous'
+            },
+            attack: {
+                type: 'jump',
+                tiles: [
+                    {x:0, y:2}
+                ]
+            }
+        },
+        2: {
+            moves: {
+                up: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                type: 'continuous'
+            },
+            attack: {
+                type: 'jump',
+                tiles: [
+                    {x:0, y:2},
+                    {x:0, y:3}
+                ]
+            }
+        },
+        3: {
+            moves: {
+                up: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                downRight: 1,
+                downLeft: 1,
+                type: 'continuous'
+            },
+            attack: {
+                type: 'jump',
+                tiles: [
+                    {x:0, y:2},
+                    {x:0, y:3},
+                    {x:1, y:2},
+                    {x:-1, y:2},
+                ]
+            }
+        }
+    },
+
+    knight: {
+        1: {
+            moves: {
+                type: 'continuous',
+                down: 1,
+                left: 1,
+                right: 1,
+                upRight: 1,
+                upLeft: 1
+            }
+        },
+        2: {
+            moves: {
+                type: 'jump',
+                tiles: [
+                    {x:1, y:2},
+                    {x:1, y:-2},
+                    {x:-1, y:2},
+                    {x:-1, y:-2},
+                    {x:2, y:1},
+                    {x:2, y:-1},
+                    {x:-2, y:1},
+                    {x:-2, y:-1},
+                ]
+            }
+        },
+        3: {
+            moves: {
+                type: 'jump',
+                tiles: [
+                    {x:1, y:2},
+                    {x:1, y:-2},
+                    {x:-1, y:2},
+                    {x:-1, y:-2},
+                    {x:2, y:1},
+                    {x:2, y:-1},
+                    {x:-2, y:1},
+                    {x:-2, y:-1},
+                    {x:1, y:0},
+                    {x:-1, y:0},
+                    {x:0, y:1},
+                    {x:0, y:-1},
+                ]
+            }
+        }  
+    },
+
+    musketeer: {
+        1: {
+            moves: {
+                type: 'continuous',
+                up: 1,
+                left: 1,
+                right: 1,
+                down: 1
+            },
+            attack: {
+                type: 'continuous',
+                up: 1
+            }
+        },
+        2: {
+            moves: {
+                type: 'continuous',
+                up: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                downRight: 1,
+                downLeft: 1
+            },
+            attack: {
+                type: 'continuous',
+                up: 2
+            }
+        },
+        3: {
+            moves: {
+                type: 'continuous',
+                up: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                downRight: 1,
+                downLeft: 1,
+                upRight: 1,
+                upLeft: 1
+            },
+            attack: {
+                type: 'continuous',
+                up: 2,
+                upRight: 1,
+                upLeft: 1
+            }
+        }
+    },
+
+    counsel: {
+        1: {
+            moves: {
+                up: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                type: 'continuous'
+            }
+        },
+        2: {
+            moves: {
+                type: 'jump',
+                tiles: [
+                    {x: 1, y:0},
+                    {x: -1, y:0},
+                    {x: 0, y:1},
+                    {x: 0, y:-1},
+
+                    {x: 2, y:2},
+                    {x: -2, y:2},
+                    {x: 2, y:-2},
+                    {x: -2, y:-2},
+                ]
+            }
+        },
+        3: {
+            moves: {
+                type: 'jump',
+                tiles: [
+                    {x: 1, y:0},
+                    {x: -1, y:0},
+                    {x: 0, y:1},
+                    {x: 0, y:-1},
+                    {x: 1, y:1},
+                    {x: -1, y:1},
+                    {x: 1, y:-1},
+                    {x: -1, y:-1},
+
+                    {x: 3, y:0},
+                    {x: -3, y:0},
+                    {x: 0, y:3},
+                    {x: 0, y:-3},
+                    {x: 3, y:3},
+                    {x: -3, y:3},
+                    {x: 3, y:-3},
+                    {x: -3, y:-3},
+                ]
+            }
+        }
+    },
+
+    samurai: {
+        1: {
+            moves: {
+                type: 'continuous',
+                upRight: 1,
+                upLeft: 1,
+                downRight: 1,
+                downLeft: 1
+            }
+        },
+        2: {
+            moves: {
+                type: 'continuous',
+                upRight: 2,
+                upLeft: 2,
+                downRight: 2,
+                downLeft: 2
+            }
+        },
+        3:{
+            moves: {
+                type: 'continuous',
+                upRight: 9,
+                upLeft: 9,
+                downRight: 9,
+                downLeft: 9
+            }
+        }
+    },
+
+    fortress: {
+        1: {
+            moves: {
+                type: 'continuous',
+                up: 1,
+                left: 1,
+                right: 1,
+                down: 1
+            }
+        },
+        2: {
+            moves: {
+                type: 'continuous',
+                up: 2,
+                left: 2,
+                right: 2,
+                down: 2
+            }
+        },
+        3:{
+            moves: {
+                type: 'continuous',
+                up: 9,
+                left: 9,
+                right: 9,
+                down: 9
+            }
+        }
+    },
+
+    cannon: {
+        1: {
+            moves: {
+                type: 'continuous',
+                up: 1,
+                left: 1,
+                right: 1,
+                down: 1
+            },
+            attack: {
+                type: 'continuous',
+                up: 2,
+            }
+        },
+        2: {
+            moves: {
+                type: 'continuous',
+                up: 1,
+                left: 1,
+                right: 1,
+                down: 1
+            },
+            attack: {
+                type: 'continuous',
+                up: 3,
+            }
+        },
+        3: {
+            moves: {
+                type: 'continuous',
+                up: 1,
+                left: 1,
+                right: 1,
+                down: 1
+            },
+            attack: {
+                type: 'continuous',
+                up: 4,
+            }
+        }
+    },
+
+    spy: {
+        1: {
+            moves: {
+                type: 'jump',
+                tiles: [
+                    {x:-2, y:0},
+                    {x:2, y:0},
+                    {x:0, y:2},
+                    {x:0, y:-2},
+                    {x:-1, y:-1},
+                    {x:1, y:1},
+                    {x:-1, y:1},
+                    {x:1, y:-1},
+                ]
+            }
+        },
+        2: {
+            moves: {
+                type: 'jump',
+                tiles: [
+                    {x:-2, y:0},
+                    {x:2, y:0},
+                    {x:0, y:2},
+                    {x:0, y:-2},
+
+                    {x:-1, y:-1},
+                    {x:1, y:1},
+                    {x:-1, y:1},
+                    {x:1, y:-1},
+
+                    {x:-2, y:-2},
+                    {x:2, y:2},
+                    {x:-2, y:2},
+                    {x:2, y:-2},
+                ]
+            }
+        },
+        3: {
+            moves: {
+                type: 'continuous',
+                up: 9,
+                upRight: 9,
+                upLeft: 9,
+                left: 9,
+                right: 9,
+                downLeft: 9,
+                downRight: 9
+            }
+        }
+    },
+
+    king: {
+        1: {
+            moves: {
+                up: 1, 
+                upRight: 1,
+                upLeft: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                downRight: 1,
+                downLeft: 1,
+                type: 'continuous'
+            }
+        },
+        2: {
+            moves: {
+                up: 1, 
+                upRight: 1,
+                upLeft: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                downRight: 1,
+                downLeft: 1,
+                type: 'continuous'
+            }
+        },
+        3: {
+            moves: {
+                up: 1, 
+                upRight: 1,
+                upLeft: 1,
+                left: 1,
+                right: 1,
+                down: 1,
+                downRight: 1,
+                downLeft: 1,
+                type: 'continuous'
+            }
+        }
+    }
+}
+
+export default moves
---
+++ src/utils/startingPieces.ts
@@ -0,0 +1,229 @@
+const initialPieces = []
+
+//Pawns
+
+for(let i=0; i<9; i++){
+    initialPieces.push({
+        id: 'white-pawn-'+i,
+        name: 'pawn',
+        team: 'white'
+    })
+}
+
+for(let i=0; i<9; i++){
+    initialPieces.push({
+        id: 'black-pawn-'+i,
+        name: 'pawn',
+        team: 'black'
+    })
+}
+
+//Major generals
+
+for(let i=0; i<4; i++){
+    initialPieces.push({
+        id: 'white-major-general-'+i,
+        name: 'majorGeneral',
+        team: 'white'
+    })
+}
+
+for(let i=0; i<4; i++){
+    initialPieces.push({
+        id: 'black-major-general-'+i,
+        name: 'majorGeneral',
+        team: 'black'
+    })
+}
+
+//lieutenant general
+
+for(let i=0; i<4; i++){
+    initialPieces.push({
+        id: 'white-lieutenant-general-'+i,
+        name: 'lieutenantGeneral',
+        team: 'white'
+    })
+}
+
+for(let i=0; i<4; i++){
+    initialPieces.push({
+        id: 'black-lieutenant-general-'+i,
+        name: 'lieutenantGeneral',
+        team: 'black'
+    })
+}
+
+//General 
+
+for(let i=0; i<5; i++){
+    initialPieces.push({
+        id: 'white-general-'+i,
+        name: 'general',
+        team: 'white'
+    })
+}
+
+for(let i=0; i<5; i++){
+    initialPieces.push({
+        id: 'black-general-'+i,
+        name: 'general',
+        team: 'black'
+    })
+}
+
+// Archer
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'white-archer-'+i,
+        name: 'archer',
+        team: 'white'
+    })
+}
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'black-archer-'+i,
+        name: 'archer',
+        team: 'black'
+    })
+}
+
+//Knight
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'white-knight-'+i,
+        name: 'knight',
+        team: 'white'
+    })
+}
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'black-knight-'+i,
+        name: 'knight',
+        team: 'black'
+    })
+}
+
+//Musketeer
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'white-musketeer-'+1,
+        name: 'musketeer',
+        team: 'white'
+    })
+}
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'black-musketeer-'+1,
+        name: 'musketeer',
+        team: 'black'
+    })
+}
+
+//Counsel
+
+initialPieces.push({
+    id: 'white-counsel-'+1,
+    name: 'counsel',
+    team: 'white'
+})
+
+initialPieces.push({
+    id: 'black-counsel-'+1,
+    name: 'counsel',
+    team: 'black'
+})
+
+//Samurai 
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'white-samurai-'+i,
+        name: 'samurai',
+        team: 'white'
+    })
+}
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'black-samurai-'+i,
+        name: 'samurai',
+        team: 'black'
+    })
+}
+
+//Fortress 
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'white-fortress-'+i,
+        name: 'fortress',
+        team: 'white'
+    })
+}
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'black-fortress-'+i,
+        name: 'fortress',
+        team: 'black'
+    })
+}
+
+//Cannon 
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'white-cannon-'+i,
+        name: 'cannon',
+        team: 'white'
+    })
+}
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'black-cannon-'+i,
+        name: 'cannon',
+        team: 'black'
+    })
+}
+
+//Spy
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'white-spy-'+i,
+        name: 'spy',
+        team: 'white'
+    })
+}
+
+for(let i=0; i<2; i++){
+    initialPieces.push({
+        id: 'black-spy-'+i,
+        name: 'spy',
+        team: 'black'
+    })
+}
+
+//Marshall
+
+initialPieces.push({
+    id: 'white-king-'+1,
+    name: 'king',
+    team: 'white'
+})
+
+initialPieces.push({
+    id: 'black-king-'+1,
+    name: 'king',
+    team: 'black'
+})
+
+export default initialPieces
